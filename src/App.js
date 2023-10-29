038a0cfb5c284dbc1ea4a9bda0d29f842ddf84e9
ej0331 <k0970133227@gmail.com>
2023-10-29 20:35:11 +0800

    chore: 🔧 cicd test

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React EJ TEST
         </a>
       </header>
     </div>
